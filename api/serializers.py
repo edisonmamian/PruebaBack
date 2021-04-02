a44b261f49d9a58d59c0c910f73e08ab7eb98790
Edison Felipe Mamián Cerón <edison.mamian@correounivalle.edu.co>
2021-04-01 21:44:50 -0500

    creación modelos

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,13 @@
 from rest_framework import serializers
+
+class StatesSerializer (serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    slug = serializers.CharField()
+
+    def create (self, validate_data):
+        pass
+
+class CitiesSerializer (serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    slug = serializers.CharField()
+    zip = serializers.CharField()
